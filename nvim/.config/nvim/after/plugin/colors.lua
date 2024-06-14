11b689e8bdc0379a4e7954f4c73cd62774810712
Adit <aditsharma1203@gmail.com>
2024-06-15 01:21:36 +0530

    initial commit with nvim

---
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -0,0 +1,7 @@
+vim.cmd("colorscheme kanagawa-dragon")
+
+--vim.cmd("colorscheme doom-oceanic-next")
+
+
+--vim.api.nvim_set_hl( 0, "Normal", { bg = "none" } )
+--vim.api.nvim_set_hl( 0, "NormalFloat", { bg = "none" } )
