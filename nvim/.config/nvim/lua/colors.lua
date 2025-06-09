1248643f57d5ab2247130e59725eab248ae80ffa
adit <aditsharma1203@gmail.com>
2025-06-09 22:52:35 +0530

    none ls broke, moved on from some particular commit

--- nvim/.config/nvim/lua/colors.lua
+++ nvim/.config/nvim/lua/colors.lua
@@ -1,6 +1,7 @@
-vim.cmd.colorscheme("kanagawa")
+vim.cmd.colorscheme("kanagawa-dragon")
 -- favs here:
 -- kanagawa
+-- kanagawa-dragon
 -- catppuccin
 -- doom-horizon
 -- catppuccin
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,6 +1,6 @@
 return {
 	"nvimtools/none-ls.nvim",
-	commit = "6be90f8cf37d7f50a89b7229412f6184b16f1b28",
+	--commit = "6be90f8cf37d7f50a89b7229412f6184b16f1b28",
 	dependencies = {
 		"nvimtools/none-ls-extras.nvim",
 	},
@@ -8,7 +8,7 @@ return {
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
-			sources = {
+		 sources = {
 				null_ls.builtins.formatting.stylua,
 
 				null_ls.builtins.formatting.prettier,
